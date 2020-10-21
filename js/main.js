3e80637b522db887fa611574728d1b08bc0124f7
VitaliiRadaiev <hf.chelenger@gmail.com>
2020-10-21 10:39:31 +0300

    add page

--- js/main.js
+++ js/main.js
@@ -266,6 +266,27 @@ function cardJournalHandler() {
 }
 
 cardJournalHandler();;
+// ==  slider ==========================================================================
+{
+	let slider = document.querySelectorAll('.reviews-slider');
+	if(slider.length>0) {
+		slider.forEach(item => {
+			var mySwiper = new Swiper(item.querySelector('.swiper-container'), {
+			slidesPerView:1,
+			autoHeight: true,
+			loop: true,
+			speed: 600,
+			spaceBetween: 15,
+			pagination: {
+			    el: item.querySelector('.swiper-pagination'),
+			    clickable: true,
+			  },
+			})
+		})
+	}
+}
+// == and  slider ==========================================================================
+;
 // === COMMON ==================================================================
 
 
@@ -387,4 +408,249 @@ cardJournalHandler();;
 ;
 // === // HOME ==================================================================
 
+// === MARQUES ==================================================================
+{
+	let menuTable = document.querySelector('.info-block__nav');
+	if(menuTable) {
+		document.querySelectorAll('.info-block__triggers').forEach((item) => {
+			item.addEventListener('click', function(e) {
+				e.preventDefault();
+				const id = e.target.getAttribute('href').replace('#','');
+
+				document.querySelectorAll('.info-block__triggers').forEach((child) => {
+					child.classList.remove('active');
+				});
+
+				document.querySelectorAll('.info-block__tabs-content').forEach((child) => {
+					child.classList.remove('active');
+				});
+
+				item.classList.add('active');
+				document.getElementById(id).classList.add('active');
+			});
+		});
+	}
+}
+;
+// === // MARQUES ==================================================================
+
+// === CONTACT ==================================================================
+//Select
+let selects = document.getElementsByTagName('select');
+if (selects.length > 0) {
+	selects_init();
+}
+function selects_init() {
+	for (let index = 0; index < selects.length; index++) {
+		const select = selects[index];
+		select_init(select);
+	}
+	//select_callback();
+	document.addEventListener('click', function (e) {
+		selects_close(e);
+	});
+	document.addEventListener('keydown', function (e) {
+		if (e.which == 27) {
+			selects_close(e);
+		}
+	});
+}
+function selects_close(e) {
+	const selects = document.querySelectorAll('.select');
+	if (!e.target.closest('.select')) {
+		for (let index = 0; index < selects.length; index++) {
+			const select = selects[index];
+			const select_body_options = select.querySelector('.select__options');
+			select.classList.remove('_active');
+			_slideUp(select_body_options, 100);
+		}
+	}
+}
+function select_init(select) {
+	const select_parent = select.parentElement;
+	const select_modifikator = select.getAttribute('class');
+	const select_selected_option = select.querySelector('option:checked');
+	select.setAttribute('data-default', select_selected_option.value);
+	select.style.display = 'none';
+
+	select_parent.insertAdjacentHTML('beforeend', '<div class="select select_' + select_modifikator + '"></div>');
+
+	let new_select = select.parentElement.querySelector('.select');
+	new_select.appendChild(select);
+	select_item(select);
+}
+function select_item(select) {
+	const select_parent = select.parentElement;
+	const select_items = select_parent.querySelector('.select__item');
+	const select_options = select.querySelectorAll('option');
+	const select_selected_option = select.querySelector('option:checked');
+	const select_selected_text = select_selected_option.text;
+	const select_type = select.getAttribute('data-type');
+
+	if (select_items) {
+		select_items.remove();
+	}
+
+	let select_type_content = '';
+	if (select_type == 'input') {
+		select_type_content = '<div class="select__value icon-select-arrow"><input autocomplete="off" type="text" name="form[]" value="' + select_selected_text + '" data-error="Ошибка" data-value="' + select_selected_text + '" class="select__input"></div>';
+	} else {
+		select_type_content = '<div class="select__value icon-select-arrow"><span>' + select_selected_text + '</span></div>';
+	}
+
+	select_parent.insertAdjacentHTML('beforeend',
+		'<div class="select__item">' +
+		'<div class="select__title">' + select_type_content + '</div>' +
+		'<div class="select__options">' + select_get_options(select_options) + '</div>' +
+		'</div></div>');
+
+	select_actions(select, select_parent);
+}
+function select_actions(original, select) {
+	const select_item = select.querySelector('.select__item');
+	const select_body_options = select.querySelector('.select__options');
+	const select_options = select.querySelectorAll('.select__option');
+	const select_type = original.getAttribute('data-type');
+	const select_input = select.querySelector('.select__input');
+
+	select_item.addEventListener('click', function () {
+		let selects = document.querySelectorAll('.select');
+		for (let index = 0; index < selects.length; index++) {
+			const select = selects[index];
+			const select_body_options = select.querySelector('.select__options');
+			if (select != select_item.closest('.select')) {
+				select.classList.remove('_active');
+				_slideUp(select_body_options, 100);
+			}
+		}
+		_slideToggle(select_body_options, 100);
+		select.classList.toggle('_active');
+	});
+
+	for (let index = 0; index < select_options.length; index++) {
+		const select_option = select_options[index];
+		const select_option_value = select_option.getAttribute('data-value');
+		const select_option_text = select_option.innerHTML;
+
+		if (select_type == 'input') {
+			select_input.addEventListener('keyup', select_search);
+		} else {
+			if (select_option.getAttribute('data-value') == original.value) {
+				select_option.style.display = 'none';
+			}
+		}
+		select_option.addEventListener('click', function () {
+			for (let index = 0; index < select_options.length; index++) {
+				const el = select_options[index];
+				el.style.display = 'block';
+			}
+			if (select_type == 'input') {
+				select_input.value = select_option_text;
+				original.value = select_option_value;
+			} else {
+				select.querySelector('.select__value').innerHTML = '<span>' + select_option_text + '</span>';
+				original.value = select_option_value;
+				select_option.style.display = 'none';
+			}
+		});
+	}
+}
+function select_get_options(select_options) {
+	if (select_options) {
+		let select_options_content = '';
+		for (let index = 0; index < select_options.length; index++) {
+			const select_option = select_options[index];
+			const select_option_value = select_option.value;
+			if (select_option_value != '') {
+				const select_option_text = select_option.text;
+				select_options_content = select_options_content + '<div data-value="' + select_option_value + '" class="select__option">' + select_option_text + '</div>';
+			}
+		}
+		return select_options_content;
+	}
+}
+function select_search(e) {
+	let select_block = e.target.closest('.select ').querySelector('.select__options');
+	let select_options = e.target.closest('.select ').querySelectorAll('.select__option');
+	let select_search_text = e.target.value.toUpperCase();
+
+	for (let i = 0; i < select_options.length; i++) {
+		let select_option = select_options[i];
+		let select_txt_value = select_option.textContent || select_option.innerText;
+		if (select_txt_value.toUpperCase().indexOf(select_search_text) > -1) {
+			select_option.style.display = "";
+		} else {
+			select_option.style.display = "none";
+		}
+	}
+}
+function selects_update_all() {
+	let selects = document.querySelectorAll('select');
+	if (selects) {
+		for (let index = 0; index < selects.length; index++) {
+			const select = selects[index];
+			select_item(select);
+		}
+	}
+};
+// === // CONTACT ==================================================================
+
 });
+
+// === GOOGLE MAP ==================================================================
+// ==== //  google map ===============
+
+{
+
+
+	let isMap = document.getElementById("map");
+	if(isMap) {
+		var map;
+
+		let center = {
+			lat: 51.735542,
+			lng: -1.666285,
+		}
+
+		let markerPosition = {
+			lat: 51.735542,
+			lng: -1.666285,
+		}
+
+		// Функция initMap которая отрисует карту на странице
+		function initMap() {
+
+			// В переменной map создаем объект карты GoogleMaps и вешаем эту переменную на <div id="map"></div>
+			map = new google.maps.Map(document.getElementById('map'), {
+				// При создании объекта карты необходимо указать его свойства
+				// center - определяем точку на которой карта будет центрироваться
+				center: {lat: center.lat, lng: center.lng},
+				// zoom - определяет масштаб. 0 - видно всю платнеу. 18 - видно дома и улицы города.
+
+				zoom: 14,
+
+				// Добавляем свои стили для отображения карты
+				//styles: 
+			});
+
+			// Создаем маркер на карте
+			var marker = new google.maps.Marker({
+
+				// Определяем позицию маркера
+			    position: {lat: markerPosition.lat, lng: markerPosition.lng},
+
+			    // Указываем на какой карте он должен появится. (На странице ведь может быть больше одной карты)
+			    map: map,
+
+			    // Пишем название маркера - появится если навести на него курсор и немного подождать
+			    title: '',
+			    label: '',
+
+			    // Укажем свою иконку для маркера
+			   // icon: 'img/contact/googlMarker.svg',
+			});
+
+		}
+	}
+};
+// === // GOOGLE MAP ==================================================================
